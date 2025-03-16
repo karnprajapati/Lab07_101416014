7a79239effb6f67f84b3e4b20736ae634db4bd1b
Karn <karnprajapaticc@gmail.com>
2025-03-16 19:45:09 -0400

    Angular added

---
+++ exercise-1/myapp/routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+// Middleware
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+/* POST route */
+router.post('/', function(req, res) {
+  console.log("Received POST data:", req.body);
+  res.send('POST received!');
+});
+
+module.exports = router;
